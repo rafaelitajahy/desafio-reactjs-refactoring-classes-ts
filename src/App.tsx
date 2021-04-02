b57240df37d93b0fcb6938ff291f1bdde6e946a9
rafaelitajahy <rafaelitajahy@gmail.com>
2021-04-02 19:36:57 -0300

    Desafio Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import Routes from './routes';
 
 import GlobalStyle from './styles/global';
 
-const App = () => (
-  <>
-    <GlobalStyle />
-    <Router>
-      <Routes />
-    </Router>
-  </>
-);
+const App = (): JSX.Element => {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes />
+      </Router>
+    </>
+  );
+}
 
 export default App;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,9 +3,9 @@ import { createGlobalStyle } from 'styled-components';
 export default createGlobalStyle`
   * {
     margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+    padding: 0;    
     outline: 0;
+    box-sizing: border-box;
   }
 
   body {
